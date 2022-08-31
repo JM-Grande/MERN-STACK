8a54e0784cf0e7684e49b26e608a628312adedb2
JM Grande <grandejmofficial@gmail.com>
2022-08-31 13:52:17 +0800

    finish backend
    test backend with postman

--- mern/backend/routes/workoutRoutes.js
+++ mern/backend/routes/workoutRoutes.js
@@ -1,39 +1,28 @@
 import express from "express";
 
-import Workout from "../models/workoutModel.js";
+//controller
+import {
+  createWorkout,
+  deleteWorkout,
+  getWorkout,
+  getWorkouts,
+  updateWorkout,
+} from "../controllers/workoutController.js";
 
 const router = express.Router();
 
 //GET all workouts
-router.get("/", (req, res) => {
-  res.json({ message: "workouts ni yow" });
-});
+router.get("/", getWorkouts);
 
 //GET workout by id
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-  res.json({ id: `${id}` });
-});
+router.get("/:id", getWorkout);
 
 // POST a new workouts
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout);
 
 // DELETE a new workouts
-router.delete("/:id", (req, res) => {
-  res.json({ message: "DELETE a new workouts" });
-});
+router.delete("/:id", deleteWorkout);
 
 // UPDATE a new workouts
-router.patch("/:id", (req, res) => {
-  res.json({ message: "UPDATE a new workouts" });
-});
+router.patch("/:id", updateWorkout);
 export default router;
